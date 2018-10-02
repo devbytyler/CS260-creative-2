b67f2d588fc7ba4e9ead5c5f3ecf39f7f5cc6a58
tylerstephens814 <tylerstephens814@gmail.com>
2018-10-02 13:49:11 -0600

    Upload and display images

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
+var imgData = '';
+
 $(document).ready(function() {
-    console.log("Hi im working")
+    $('#imageInput').change((event)=>{
+        file = event.target.files[0]
+        if (file) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                $('#uploaded')
+                .attr('src', e.target.result)
+                .width(150)
+                .height(200);
+                // console.log(e.target.result.substring(0,1000))
+            };
+            reader.readAsDataURL(file);
+        }
+    })
 })
